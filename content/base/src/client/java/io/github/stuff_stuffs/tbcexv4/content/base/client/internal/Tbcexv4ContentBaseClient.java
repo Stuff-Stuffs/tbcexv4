23d0d5ab6cc50a285fb6cd5d9422c4b3ddf8a642
Stuff-Stuffs <stevenziehlke@gmail.com>
2024-01-22 22:31:03 -0600

    Multi project

---
+++ content/base/src/client/java/io/github/stuff_stuffs/tbcexv4/content/base/client/internal/Tbcexv4ContentBaseClient.java
@@ -0,0 +1,10 @@
+package io.github.stuff_stuffs.tbcexv4.content.base.client.internal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Tbcexv4ContentBaseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ content/base/src/main/java/io/github/stuff_stuffs/tbcexv4/content/base/common/internal/Tbcexv4ContentBase.java
@@ -0,0 +1,10 @@
+package io.github.stuff_stuffs.tbcexv4.content.base.common.internal;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tbcexv4ContentBase implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
